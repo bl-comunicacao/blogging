f8c47c0eb73004ed8f895e45fc74ba1b1b28373a
RaphaelJsx <raphaelalvoliv@gmail.com>
2026-01-15 19:19:36 -0300

    feat: Removi alguns tratamentos de erros que não estavam sendo utilizados na API

--- src/errors/index.js
+++ src/errors/index.js
@@ -5,6 +5,4 @@ module.exports = {
   AppError: require('./AppError'),
   NotFoundError: require('./NotFoundError'),
   ValidationError: require('./ValidationError'),
-  UnauthorizedError: require('./UnauthorizedError'),
-  ForbiddenError: require('./ForbiddenError'),
 };
